31e7ca56059fcf11a368650e79c343061cd41e36
jiakwang <jiakang.wang@stonybrook.edu>
2020-11-12 01:25:24 -0500

    DisplayPlaylist w/ TODOs
    Delete playlist is not implemented, require a popup

--- client/src/components/PlaylistPage/DisplayPlaylist.js
+++ client/src/components/PlaylistPage/DisplayPlaylist.js
@@ -10,101 +10,100 @@ import Button from 'react-bootstrap/Button'
 import { Grid, Paper } from '@material-ui/core';
 import { pink } from '@material-ui/core/colors';
 import { AiFillPlusCircle, AiOutlinePlusCircle } from "react-icons/ai";
-/*
-class playPauseButton extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {isToggleOn: true};
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
-      }));
-    }
-  
-    render() {
-      return (
-        <button onClick={this.handleClick} className={this.state.isToggleOn?"playButton":"pausedButton"}>
-        </button>
-      );
-    }
-  }*/
+import { BsTrashFill } from "react-icons/bs";
+import gql from 'graphql-tag'
+import MusicCard from './MusicCard'
+import {Query} from 'react-apollo'
 
+const GET_LIST_DETAIL = gql`
+    query musicList($musicListId: String) {
+        musicList(id: $musicListId) {
+            musicListName
+            musics{
+                _id
+            }
+            owner{
+                _id
+            }
+        }
+    }
+`;
+
+const GET_PLAYLIST = gql`
+    query user($userId: String) {
+        user(id: $userId) {
+            nickName
+        }
+    }
+`;
 
 class DisplayPlaylistScreen extends Component{
     constructor(props){
         super(props);
     }
+
+    handleOnClick = () =>{
+        //a popup confirm deletion
+    }
     render(){
+        console.log(this.props.match.params.id);
+        let deleteButton = null;
         return(
             <div>
-                <Container>
-                    <Row>
-                        <img src={Image} width={175} height={175} alt="">
-                        </img>
-                        <Col>
-                            <Col xs={10} className="content-left">
-                                <h3>Playlists</h3>
-                                <h1 style={{fontWeight: "bold"}} >NEW Playlists</h1> {/*List name*/}
-                            </Col>
-                            <Col xs={10} className="content-center">
-                                <h5>Artirst</h5>Artirst                    
-                                <h3 style={{fontWeight: "bold"}} >Mixtapez|0 Song|0 second</h3> {/*List name*/}
-                            </Col>
-                        </Col>
-                    </Row>
-                    <Row xs={10}>
-                        <IconContext.Provider value={{ color: "#F06E9C", size: '50px' }}>
-                            <MdPauseCircleOutline/>
-                            <AiOutlinePlusCircle/>
-                            <MdMoreHoriz/>
-                        </IconContext.Provider>
-                    </Row>
-                    <Col xs={10} className="content-center">
-                        <Grid container spacing={0}>
-                            <Grid item xs ={1}>
-                                <div className='music-in-list-info'>1</div>
-                            </Grid>
-                            <Grid item xs ={5}>
-                                <div className='music-in-list-info'>Come Together</div>
-                            </Grid>
-                            <Grid item xs ={2}>
-                                <div className='music-in-list-info'>The Beatles</div>
-                            </Grid>
-                            <Grid item xs ={2}>
-                                <div className='music-in-list-info'>4:18</div>
-                            </Grid>
-                            <Grid item xs ={1}>
-                                <IconContext.Provider value={{ color: "#F06E9C", size: '30px' }}>
-                                    <MdMoreHoriz/>
-                                </IconContext.Provider>
-                            </Grid>
-                        </Grid>
-                        <Grid container spacing={0}>
-                            <Grid item xs ={1}>
-                                <div className='music-in-list-info'>2</div>
-                            </Grid>
-                            <Grid item xs ={5}>
-                                <div className='music-in-list-info'>Come Together</div>
-                            </Grid>
-                            <Grid item xs ={2}>
-                                <div className='music-in-list-info'>The Beatles</div>
-                            </Grid>
-                            <Grid item xs ={2}>
-                                <div className='music-in-list-info'>4:18</div>
-                            </Grid>
-                            <Grid item xs ={1}>
-                                <IconContext.Provider value={{ color: "#F06E9C", size: '30px' }}>
-                                    <MdMoreHoriz/>
-                                </IconContext.Provider>
-                            </Grid>
-                        </Grid>
-                    </Col>
-                </Container>
+                <Query query={GET_LIST_DETAIL} variables={{musicListId: this.props.match.params.id}}>
+                    {({loading, error, data}) =>{
+                        if (loading) return 'Loading...';
+                        if (error) return `Error! ${error.message}`;
+                        let numberOfMusics = data.musicList.musics.length;
+                        if (this.props.userId === data.musicList.owner._id) {
+                            deleteButton = <BsTrashFill onClick={this.handleOnClick}/>
+                        }
+                        return(
+                            <div>
+                                <Row>
+                                    <img src={Image} width={175} height={175} alt="">
+                                    </img>
+                                    <Col>
+                                        <Col xs={10} className="content-left">
+                                            <h3>Playlists</h3>
+                                            <h1 style={{fontWeight: "bold"}} >{data.musicList.musicListName} </h1> 
+                                        </Col>
+                                        <Col xs={10} className="content-center">
+                                            <Query query={GET_PLAYLIST} variables={{userId: data.musicList.owner._id}}>
+                                                {({loading, error, data}) => 
+                                                    {                                                    
+                                                        if (loading) return 'Loading...';
+                                                        if (error) return `Error! ${error.message}`;
+                                                        console.log(data);
+                                                        return(
+                                                            <div>
+                                                                <h5>Artirst</h5>                    
+                                                                <h3 style={{fontWeight: "bold"}} >{data.user.nickName}|{numberOfMusics} Song|0 second</h3>
+                                                            </div>
+                                                        )
+                                                    }
+                                                }
+                                            </Query>                                            
+                                        </Col>
+                                    </Col>
+                                </Row>
+                                <Row xs={10}>
+                                    <IconContext.Provider value={{ color: "#F06E9C", size: '50px' }}>
+                                        <MdPauseCircleOutline/>
+                                        <AiOutlinePlusCircle/>
+                                        {deleteButton}
+                                        <MdMoreHoriz/>
+                                    </IconContext.Provider>
+                                </Row>   
+                                                         
+                                <MusicCard musics={data.musicList.musics}/>
+                            </div>
+                        )
+                    }                                   
+                    }
+                </Query>              
             </div>
+           
         )
     };
 
--- client/src/components/PlaylistPage/MusicCard.js
+++ client/src/components/PlaylistPage/MusicCard.js
@@ -4,28 +4,75 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Link } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
+import { IconContext } from "react-icons";
+import { MdMoreHoriz} from "react-icons/md";
+import gql from 'graphql-tag'
+import {Query} from 'react-apollo'
+import Button from 'react-bootstrap/Button'
 
+const GET_MUSIC_DETAIL = gql`
+    query music($musicId: String) {
+        music(id: $musicId) {
+            musicName
+            artist
+        }
+    }
+`;
 class MusicCard extends Component {
     constructor(props){
         super(props);
     }
     render() {
-        var playlistID = this.props.musicListId;
-        var link = "/playlist/"+playlistID;
+        let itr = this.makeRangeIterator();
+        let listblock = this.props.musics.map(musicId => <Query query={GET_MUSIC_DETAIL} variables={{musicId: musicId}}>
+            {({loading, error, data}) =>{
+                if (loading) return 'Loading...';
+                if (error) return `Error! ${error.message}`;
+                console.log(data);
+                return(
+                    <Grid container spacing={0}>
+                        <Grid item xs ={1}>
+                            <div className='music-list-header'>{itr.next()}</div>
+                        </Grid>
+                        <Grid item xs ={5}>
+                            <div className='music-list-header'>{data.music.musicName} </div>
+                        </Grid>
+                        <Grid item xs ={2}>
+                            <div className='music-list-header'>{data.music.artist}</div>
+                        </Grid>
+                        <Grid item xs ={2}>
+                            <div className='music-list-header'>Length</div>
+                        </Grid>
+                        <Grid item xs ={1}>
+                            <IconContext.Provider value={{ color: "#F06E9C", size: '30px' }}>
+                                <MdMoreHoriz/>
+                            </IconContext.Provider>
+                        </Grid>
+                    </Grid> 
+                )
+            }}
+        </Query>);
+        if(this.props.musics.length == 0){
+            listblock = <div>
+                <Link to='/search'>
+                    <Button className='nav-btn'>This list is empty, find more songs</Button>
+                </Link>
+            </div>
+        }
         return (
             <Col xs={10} className="content-center">
                 <Grid container spacing={0}>
                     <Grid item xs ={1}>
-                        <div className='music-in-list-info'>1</div>
+                        <div className='music-list-header'>Number</div>
                     </Grid>
                     <Grid item xs ={5}>
-                        <div className='music-in-list-info'>Come Together</div>
+                        <div className='music-list-header'>Music</div>
                     </Grid>
                     <Grid item xs ={2}>
-                        <div className='music-in-list-info'>The Beatles</div>
+                        <div className='music-list-header'>Artist</div>
                     </Grid>
                     <Grid item xs ={2}>
-                        <div className='music-in-list-info'>4:18</div>
+                        <div className='music-list-header'>Length</div>
                     </Grid>
                     <Grid item xs ={1}>
                         <IconContext.Provider value={{ color: "#F06E9C", size: '30px' }}>
@@ -33,27 +80,28 @@ class MusicCard extends Component {
                         </IconContext.Provider>
                     </Grid>
                 </Grid>
-                <Grid container spacing={0}>
-                    <Grid item xs ={1}>
-                        <div className='music-in-list-info'>2</div>
-                    </Grid>
-                    <Grid item xs ={5}>
-                        <div className='music-in-list-info'>Come Together</div>
-                    </Grid>
-                    <Grid item xs ={2}>
-                        <div className='music-in-list-info'>The Beatles</div>
-                    </Grid>
-                    <Grid item xs ={2}>
-                        <div className='music-in-list-info'>4:18</div>
-                    </Grid>
-                    <Grid item xs ={1}>
-                        <IconContext.Provider value={{ color: "#F06E9C", size: '30px' }}>
-                            <MdMoreHoriz/>
-                        </IconContext.Provider>
-                    </Grid>
-                </Grid>
+                {listblock}
             </Col>
         );
+    }
+
+    makeRangeIterator = (start = 0, end = Infinity, step = 1) => {
+        let nextIndex = start;
+        let iterationCount = 0;
+    
+        const rangeIterator = {
+           next: function() {
+               let result;
+               if (nextIndex < end) {
+                   result = { value: nextIndex, done: false }
+                   nextIndex += step;
+                   iterationCount++;
+                   return result;
+               }
+               return { value: iterationCount, done: true }
+           }
+        };
+        return rangeIterator;
     }
 }
 
