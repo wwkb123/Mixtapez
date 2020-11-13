3afd27dfed47e88d7bdc95c54ff2c7923125326a
Yiu Chung Yau <wwkb1233@gmail.com>
2020-11-12 20:38:44 -0500

    add dropdown menu for song card

--- client/src/components/SongCard.js
+++ client/src/components/SongCard.js
@@ -5,59 +5,115 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import data from './Mixtapez_data.json'
+import {MdMoreHoriz} from "react-icons/md";
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-class SongCard extends Component{
-    
-    render() {
-        var song = this.props.song;
-        if(song){
-            var minutes = 0;
-            minutes = Math.round((song.duration_ms/1000) / 60);
-            if(minutes < 10) minutes = "0"+minutes;
-            var seconds = 0;
-            seconds = Math.round((song.duration_ms/1000) % 60);
-            if(seconds < 10) seconds = "0"+seconds;
-            var artist = "N/A";
-            if(song.artists){
-                artist = song.artists[0].name
-            }
-            var album = "N/A";
-            if(song.album){
-                album = song.album.name;
-            }
-            return(
-                <div>
-                    <Container>
-                        <Row className="border-bottom-accent">
-                            <Col xs={1}>
-                                &#9825;
-                            </Col>
-                            <Col xs={3}>
-                                {/* {data.music[id].musicName} */}
-                                { song.name }
-                            </Col>
-                            <Col xs={2}>
-                                {/* {data.music[id].artist} */}
-                                { artist }
-                            </Col>
-                            <Col xs={3}>
-                                {/* {data.music[id].album} */}
-                                { album }
-                            </Col>
-                            <Col xs={2}>
-                                {/* 0{data.music[id].length / 60}:{data.music[id].length % 60}0 */}
-                                { minutes }:{ seconds }
-                            </Col>
-                            <Col xs={1}>
-                                ...
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
-            );
-        }else{
-            return <div>error</div>
+
+const options = [
+    'Add to Queue',
+    'Save to Liked Songs',
+    'Add to Playlist',
+    'Share'
+  ];
+
+export default function SongCard(props){
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+        if(index === 0){  // add to queue
+            
+        }else if(index === 1){ // add to liked songs
+
+        }else if(index === 2){ // add to playlist
+
+        }else if(index === 3){ // share
+
         }
+    };
+
+    var song = props.song;
+    if(song){
+        var minutes = 0;
+        minutes = Math.round((song.duration_ms/1000) / 60);
+        if(minutes < 10) minutes = "0"+minutes;
+        var seconds = 0;
+        seconds = Math.round((song.duration_ms/1000) % 60);
+        if(seconds < 10) seconds = "0"+seconds;
+        var artist = "N/A";
+        if(song.artists){
+            artist = song.artists[0].name
+        }
+        var album = "N/A";
+        if(song.album){
+            album = song.album.name;
+        }
+        return(
+            <div>
+                <Container>
+                    <Row className="border-bottom-accent">
+                        <Col xs={1}>
+                            &#9825;
+                        </Col>
+                        <Col xs={3}>
+                            {/* {data.music[id].musicName} */}
+                            { song.name }
+                        </Col>
+                        <Col xs={2}>
+                            {/* {data.music[id].artist} */}
+                            { artist }
+                        </Col>
+                        <Col xs={3}>
+                            {/* {data.music[id].album} */}
+                            { album }
+                        </Col>
+                        <Col xs={2}>
+                            {/* 0{data.music[id].length / 60}:{data.music[id].length % 60}0 */}
+                            { minutes }:{ seconds }
+                        </Col>
+                        <Col xs={1}>
+                        <IconButton
+                            aria-label="more"
+                            aria-controls="menu"
+                            aria-haspopup="true"
+                            onClick={handleClick}
+                        >
+                            <MdMoreHoriz/>
+                        </IconButton>
+                            <Menu
+                                id="menu"
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                {options.map((option, index) => (
+                                <MenuItem
+                                    key={option}
+                                    onClick={(event) => handleMenuItemClick(event, index)}
+                                >
+                                    {option}
+                                </MenuItem>
+                                ))}
+                            </Menu>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }else{
+        return <div>error</div>
     }
 }
-export default SongCard;
