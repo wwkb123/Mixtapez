0622e04b18b90fed6020de23c283a12ec62f8d50
Yiu Chung Yau <wwkb1233@gmail.com>
2020-11-19 21:30:57 -0500

    udpate styles

--- client/src/components/HomePage/AlbumCard.js
+++ client/src/components/HomePage/AlbumCard.js
@@ -10,7 +10,7 @@ class AlbumCard extends Component {
     render() {
         return (
             <div>
-                <Link to="/playlist/1">
+                <Link to="/playlist/1" style={{'color':'#ed4e85'}}>
                     <div className="album-card">
                         <img src={Image} width={100} height={100} alt=""/>
                         <div>Album</div>
