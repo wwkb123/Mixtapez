456497f75412747a0c5875ea82a131c55b8fffc5
Yiu Chung Yau <wwkb1233@gmail.com>
2020-12-03 15:20:43 -0500

    add friend requests accordion

--- client/src/components/FriendPage/FriendRequestsScreen.js
+++ client/src/components/FriendPage/FriendRequestsScreen.js
@@ -4,17 +4,61 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import FriendRequestCard from './FriendRequestCard.js'
 import { Link } from 'react-router-dom';
+import UserAPI from '../../apis/UserAPI.js';
 
 class FriendRequestsScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            friendRequests: [],
+            isLoaded: false
+        }
+    }
+
+    getFriendRequests = () => {
+        var friendRequests = this.props.location.friendRequests;
+        console.log(friendRequests);
+        this.setState({friendRequests: friendRequests});
+        this.setState({isLoaded: true});
+    }
+
+    componentDidMount() {
+        this.getFriendRequests();
+    }
 
     render() {
-        return (
-            <div>
-                <br/>
-                <h1>Friend Requests (1)</h1>
-                <FriendRequestCard/>
-            </div>
-        );
+        var friendRequests = ""
+        var friend_request_cards = ""
+        if(this.state.isLoaded){
+            console.log(this.state.friendRequests, this.state.friendRequests==true)
+            if(this.state.friendRequests){
+                friendRequests = this.state.friendRequests;
+                console.log(friendRequests.length, "asd");
+                if(friendRequests.length == 0){
+                    friend_request_cards = <div>You have no friend requests.</div>
+                }else{
+                    
+                    friend_request_cards = friendRequests.map((user, index) => {
+                        return (<div>{user}</div>)
+                    })
+                }
+            }
+            return (
+                <div>
+                    <br/>
+                    <h1>Friend Requests ({friendRequests.length})</h1>
+                    { friend_request_cards }
+                    {/* <FriendRequestCard/> */}
+                </div>
+            );
+        }else{
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
+        }
+        
     }
 }
 
--- client/src/components/FriendPage/FriendScreen.js
+++ client/src/components/FriendPage/FriendScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -6,6 +7,8 @@ import FriendCard from './FriendCard.js'
 import { Link } from 'react-router-dom';
 import data from '../Mixtapez_data.json'
 import UserAPI from "../../apis/UserAPI";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 class FriendScreen extends Component {
     constructor(props){
@@ -41,15 +44,68 @@ class FriendScreen extends Component {
         this.getFriendsAndRequests();
     }
 
+    onAccordionClick = () => {
+        var accordion1 = document.getElementById("accordion1");
+        if(accordion1)
+            accordion1.classList.toggle("active");
+        var panel = document.getElementById("friend_requests_panel");
+        if(panel){
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
+    }
+
     render() {
+        var friend_cards = ""
+        var friendRequests = ""
+        var friend_request_cards = ""
         if(this.state.isLoaded){
+            if(this.state.friends){
+                if(this.state.friends.length == 0){
+                    friend_cards = <div>You have no friend yet. Go to search page and search for users.</div>
+                }else{
+                    friend_cards = this.state.friends.map((friend, index) => {
+                        return (<div>{friend}</div>)
+                    })
+                }
+                
+            }
+            if(this.state.friendRequests){
+                friendRequests = this.state.friendRequests;
+                if(friendRequests.length == 0){
+                    friend_request_cards = <div>You have no friend requests.</div>
+                }else{
+                    friend_request_cards = friendRequests.map((user, index) => {
+                        return (<div>{user}</div>)
+                    })
+                }
+            }
             return (
                 <div>
                     <br/>
                     <h1>Friends</h1>
-                    <Link to="/friendrequests"><h4>Friend Requests (1)</h4></Link>
-                    <Link to="/chat/1"><FriendCard name={data.users[1].nickName}/></Link>
-                    <FriendCard name={data.users[2].nickName}/>
+                    <button className="accordion" id="accordion1" onClick={this.onAccordionClick}>
+                        <Row>
+                            <Col xs={11}>
+                                <h4>Friend Requests ({friendRequests.length})</h4>
+                            </Col>
+                            <Col xs={1}>
+                                <IconContext.Provider value={{ color: "#F06E9C", size: '30px' }}>
+                                    <BsFillCaretDownFill/>
+                                </IconContext.Provider>
+                            </Col>
+                        </Row>
+                    </button>
+                    <div className="panel" id="friend_requests_panel">
+                        {friend_request_cards}
+                    </div>
+                    <hr></hr>
+                    { friend_cards }
+                    {/* <Link to="/chat/1"><FriendCard name={data.users[1].nickName}/></Link>
+                    <FriendCard name={data.users[2].nickName}/> */}
                 </div>
             );
         }else{
