9e4ee224dabd3eab64b354c759284093f4dc8804
Yiu Chung Yau <wwkb1233@gmail.com>
2020-12-05 18:41:35 -0500

    move www code to app.js, make app.js the server file

--- server/app.js
+++ server/app.js
@@ -9,6 +9,10 @@ var schema = require('./graphql/ummSchemas');
 var cors = require("cors");
 var nodemailer = require('nodemailer');  // to send emails
 
+var debug = require('debug')('server:server');
+var http = require('http');
+var port = normalizePort(process.env.PORT || '3001');
+
 const uri = "mongodb+srv://admin:admin@cluster0.wknfy.mongodb.net/db?retryWrites=true&w=majority";
 // 'mongodb://localhost/node-graphql'
 mongoose.connect(uri, { promiseLibrary: require('bluebird'), useNewUrlParser: true })
@@ -1027,4 +1031,64 @@ app.use(function(err, req, res, next) {
 function handleError(err){
   console.log(err);
 }
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+app.set('port', port);
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
 module.exports = app;
