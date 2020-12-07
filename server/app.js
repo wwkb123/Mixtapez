55240874264f657602b1415629f2b6192412a068
Yiu Chung Yau <wwkb1233@gmail.com>
2020-12-07 01:49:47 -0500

    update package.json for deploying

--- server/app.js
+++ server/app.js
@@ -65,7 +65,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Static files
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
